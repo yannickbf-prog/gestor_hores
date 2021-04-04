afb6f4a8c7b47958e32c2267b22f5575017c372b
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2021-04-04 20:19:23 +0200

    comitejo amb eliminar funcionant

--- resources/views/type_bag_hours/index.blade.php
+++ resources/views/type_bag_hours/index.blade.php
@@ -3,26 +3,28 @@
 @section('title', 'Control panel - Type bag hours')
 
 @section('content')
-    <h1>Home</h1>
-    <p>lorem ipsumm</p>
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Details</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($data as $key => $value)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $value->name }}</td>
-            <td>{{ \Str::limit($value->description, 100) }}</td>
-            <td>
-                <form action="{{ route('type-bag-hours.destroy',$value->id) }}" method="POST">   
-                  
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>  
+<h1>Home</h1>
+<p>lorem ipsumm</p>
+<table class="table table-bordered">
+    <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Details</th>
+        <th width="280px">Action</th>
+    </tr>
+    @foreach ($data as $key => $value)
+    <tr>
+        <td>{{ ++$i }}</td>
+        <td>{{ $value->name }}</td>
+        <td>{{ \Str::limit($value->description, 100) }}</td>
+        <td>
+            <form action="{{ route('type-bag-hours.destroy',$value->id) }}" method="POST">   
+                @csrf
+                @method('DELETE')      
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>  
 @endsection
