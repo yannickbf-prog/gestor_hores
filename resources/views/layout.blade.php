fce1b0c5f4b66c6c13839748017624ea8e2d0b25
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2021-05-06 19:03:12 +0200

    comitejo avans de continuar amb heders stiky

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\DB;
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         
         <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" />
-        <style>
-   
-         
-        </style>
+      
     </head>
 
     <body>
