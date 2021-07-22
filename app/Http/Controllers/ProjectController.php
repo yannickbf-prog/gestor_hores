9692607b97d87b802c8aa8008beea62926e4901c
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2021-07-22 16:24:04 +0200

    query per taula  ok amb hores totals a un projecte i hores contrctades si en te

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -73,8 +73,10 @@ class ProjectController extends Controller {
         $new_data = DB::table('hours_entry')
                 ->join('users_projects', 'users_projects.id', '=', 'hours_entry.user_project_id')
                 ->join('projects', 'projects.id', '=', 'users_projects.project_id')
+                ->join('customers', 'customers.id', '=', 'projects.customer_id')
                 ->leftJoin('bag_hours', 'bag_hours.project_id', '=', 'projects.id')
-                ->select('projects.name as project_name', DB::raw('SUM(hours_entry.hours_imputed) as total_hours_project'), 'bag_hours.contracted_hours')
+                ->select('projects.name as project_name', DB::raw('SUM(hours_entry.hours_imputed) as total_hours_project'), 'bag_hours.contracted_hours',
+                        'customers.name as customer_name', 'projects.active as project_active', 'projects.description as project_description', 'projects.created_at')
                 ->groupBy('projects.id')
                 ->get();
         
