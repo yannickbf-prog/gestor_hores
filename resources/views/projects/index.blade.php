dc826d4ba99e0f9b7eeee102ba32c28cf4155ca5
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2021-07-28 18:03:44 +0200

    crec que funciona tot be

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -537,34 +537,11 @@ $hours_left_count = 0;
             return e.id == projectId;
         });
         
-//        let customerId = document.getElementById('customerSelect').value;
-//        if(projectId == "%" && customerId != "%") {
-//            
-//            let customer = $.grep(customers_json, function (e) {
-//                return e.id == customerId;
-//            });
-//            
-//            let customerSelectHtml = document.createElement("select");
-//            //userSelectHtml.setAttribute("onchange", "loadUsersOfProject()");
-//            customerSelectHtml.setAttribute("id", "customerSelect");
-//            customerSelectHtml.setAttribute("onchange", "changeCustomer()");
-//            
-//            let option = document.createElement("option");
-//            option.value = "%";
-//            option.innerText = "{{__('message.all_m')}}";
-//            customerSelectHtml.appendChild(option);
-//            
-//            let option2 = document.createElement("option");
-//            option2.value = customer[0].customer_id;
-//            option2.innerText = customer[0].name;
-//            customerSelectHtml.appendChild(option2);
-//
-//            document.getElementById('customerSelect').remove();
-//            document.getElementById('customerGroup').appendChild(customerSelectHtml);
-//            
-//        }
-//
-//        else if...
+        let customerId = document.getElementById('customerSelect').value;
+        if(projectId == "%" && customerId != "%") {
+            
+        }
+
         if (project.length == 0) {
             let customerSelectHtml = document.createElement("select");
             //userSelectHtml.setAttribute("onchange", "loadUsersOfProject()");
@@ -634,7 +611,6 @@ $hours_left_count = 0;
             document.getElementById('stateSelect').remove();
             document.getElementById('stateGroup').appendChild(stateSelectHtml);
             
-           
            
         }
     }
