55ce4458ed06f13fce1228b3a58dfc23cfdc51a2
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2021-08-03 17:12:32 +0200

    avans de continuar

--- resources/views/bag_hours/index.blade.php
+++ resources/views/bag_hours/index.blade.php
@@ -25,7 +25,7 @@
 </div>
 <form action="{{ route($lang.'_bag_hours.index') }}" method="GET">
     @csrf
-    
+
     <div class="row py-2">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
@@ -155,73 +155,76 @@
     </div>
 </div>
 
-<table class="table table-bordered">
+<table class="table">
     @if (count($data) > 0)
-    <tr>
-        <th>Nº</th>
-        <th>{{ __('message.project') }}</th>
-        <th>{{ __('message.customer_name') }}</th>
-        <th>{{ __('message.type') }}</th>
-        <th>{{ __('message.contracted_hours') }}</th>
-        <th>{{ __('message.hours_available') }}</th>
-        <th>{{ __('message.hour_price') }}</th>
-        <th>{{ __('message.total_price') }}</th>
-        <th>{{ __('message.created_at') }}</th>
-        <th>{{ __('message.action') }}</th>
-    </tr>
+    <thead>
+        <tr class="thead-light">
+            <th>Nº</th>
+            <th>{{ __('message.project') }}</th>
+            <th>{{ __('message.customer_name') }}</th>
+            <th>{{ __('message.type') }}</th>
+            <th>{{ __('message.contracted_hours') }}</th>
+            <th>{{ __('message.hours_available') }}</th>
+            <th>{{ __('message.hour_price') }}</th>
+            <th>{{ __('message.total_price') }}</th>
+            <th>{{ __('message.created_at') }}</th>
+            <th>{{ __('message.action') }}</th>
+        </tr>
+    </thead>
     @endif
-    @forelse ($data as $key => $value)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $value->project_name }}</td>
-        <td>{{ $value->customer_name }}</td>
-        <td>{{ $value->type_name }}</td>
-        <td>{{ $value->contracted_hours }}h</td>
-        <td>{{ $value->contracted_hours - $value->total_hours_project }}h</td>
-        <td>{{ number_format($value->type_hour_price, 2, ',', '.') }}€</td>
-        <td style="width:100px">{{ number_format($value->total_price, 2, ',', '.') }}€</td>
-        <td>{{ $value->created_at->format('d/m/y') }}</td>
-        <td>
-            <form action="{{ route('projects.destroy',[$value->id, $lang]) }}" method="POST"> 
-                <a class="btn btn-primary" href="{{ route($lang.'_projects.edit',$value->id) }}">{{ __('message.edit') }}</a>
-                @csrf
-                @method('DELETE')      
-                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-                    {{ __('message.delete') }}
-                </button>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">{{ __('message.delete') }} {{ $value->name }}</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                {{ __('message.confirm') }} {{ __('message.delete') }} {{ __('message.the') }} {{ __("message.project") }} <b>{{ $value->name }}</b>?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">{{ __('message.close') }}</button>
-                                <button type="submit" class="btn btn-success">{{ __('message.delete') }}</button>
+    <tbody>
+        @forelse ($data as $key => $value)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $value->project_name }}</td>
+            <td>{{ $value->customer_name }}</td>
+            <td>{{ $value->type_name }}</td>
+            <td>{{ $value->contracted_hours }}h</td>
+            <td>{{ $value->contracted_hours - $value->total_hours_project }}h</td>
+            <td>{{ number_format($value->type_hour_price, 2, ',', '.') }}€</td>
+            <td style="width:100px">{{ number_format($value->total_price, 2, ',', '.') }}€</td>
+            <td>{{ $value->created_at->format('d/m/y') }}</td>
+            <td>
+                <form action="{{ route('projects.destroy',[$value->id, $lang]) }}" method="POST"> 
+                    <a class="btn btn-primary" href="{{ route($lang.'_projects.edit',$value->id) }}">{{ __('message.edit') }}</a>
+                    @csrf
+                    @method('DELETE')      
+                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                        {{ __('message.delete') }}
+                    </button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">{{ __('message.delete') }} {{ $value->name }}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    {{ __('message.confirm') }} {{ __('message.delete') }} {{ __('message.the') }} {{ __("message.project") }} <b>{{ $value->name }}</b>?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">{{ __('message.close') }}</button>
+                                    <button type="submit" class="btn btn-success">{{ __('message.delete') }}</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                
-            </form>
-        </td>
-    </tr>
-    @empty
+
+                </form>
+            </td>
+        </tr>
+        @empty
     <li>{{__('message.no')}} {{__('message.bags_of_hours')}} {{__('message.to_show')}}</li>
     @endforelse
-
+</tbody>
 </table> 
 <div id="paginationContainer">
     {!! $data->links() !!} 
 </div>
 @endsection
 @section('js')
-    <script type="text/javascript" src="{{ URL::asset('js/hour_bags_index.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/hour_bags_index.js') }}"></script>
 @endsection
