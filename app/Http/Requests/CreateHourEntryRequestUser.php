cbc656a95f562f1b184a1028099bfb960f4075a5
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2021-11-15 11:38:39 +0100

    avans de pasar projecte a el Pablo

--- app/Http/Requests/CreateHourEntryRequestUser.php
+++ app/Http/Requests/CreateHourEntryRequestUser.php
@@ -41,7 +41,7 @@ class CreateHourEntryRequestUser extends FormRequest {
             "inputed_hours" => "required|array",
             "inputed_hours.*" => "required|int|min:1|max:24",
             "desc" => "required|array",
-            "desc.*" => "required|string|min:5|max:150",
+            "desc.*" => "required|string|min:5|max:1000",
         ];
     }
     
--- app/Http/Requests/EditUserHourEntryRequest.php
+++ app/Http/Requests/EditUserHourEntryRequest.php
@@ -46,7 +46,7 @@ class EditUserHourEntryRequest extends FormRequest
             //"inputed_hours" => "required|array",
             //"inputed_hours.*" => "required|int|min:1|max:24",
             "desc" => "required|array",
-            "desc.*" => "required|string|min:5|max:150",
+            "desc.*" => "required|string|min:5|max:1000",
         ];
     }
     
